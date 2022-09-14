de97c3f1b25b394562c520519276f76a52975853
egustavsson <e.gustavsson@ucl.ac.uk>
2022-09-14 14:43:26 +0100

    remove unused script

--- scripts/pseudogenes/plot_pseudogene_types.R
+++ scripts/pseudogenes/plot_pseudogene_types.R
@@ -8,32 +8,14 @@ library(ggrepel)
 
 # Load data ---------------------------------------------------------------
 
-GENCODEv38 <- 
-  rtracklayer::import(here::here("data", "GENCODE_annotations/gencode.v38.annotation.gtf.gz"))
+load(here::here("results", "pseudogenes", "pseudogene_annotated.rda"))
 
 # Main --------------------------------------------------------------------
 
 GENCODEv38_pseudogenes_to_plot <-
-  GENCODEv38 %>% 
-  plyranges::filter(str_detect(gene_type, "pseudogene") & type == "gene") %>%
-  plyranges::mutate(pseudogene_type = case_when(gene_type %in% c("IG_V_pseudogene", 
-                                                                 "IG_C_pseudogene",
-                                                                 "IG_J_pseudogene",
-                                                                 "IG_pseudogene") ~ "IG",
-                                                gene_type %in% c("TR_J_pseudogene",
-                                                                 "TR_V_pseudogene") ~ "TR",
-                                                gene_type %in% c("transcribed_unprocessed_pseudogene",
-                                                                 "unprocessed_pseudogene",
-                                                                 "translated_unprocessed_pseudogene") ~ "Unprocessed",
-                                                gene_type %in% c("processed_pseudogene",
-                                                                 "transcribed_processed_pseudogene",
-                                                                 "translated_processed_pseudogene") ~ "Processed",
-                                                gene_type %in% c("transcribed_unitary_pseudogene",
-                                                                 "unitary_pseudogene") ~ "Unitary",
-                                                gene_type %in% c("polymorphic_pseudogene",
-                                                                 "rRNA_pseudogene",
-                                                                 "pseudogene") ~ "Other")) %>% 
-  data.frame() %>% 
+  pseudogene_annotated %>% 
+  dplyr::select(gene_id, pseudogene_type) %>% 
+  unique() %>% # have to unique() since some genes are duplicated when left_join() with GTEx data. This is due to PAR1 region.
   group_by(pseudogene_type) %>% 
   tally() %>% 
   dplyr::mutate(csum = rev(cumsum(rev(n))),
@@ -50,29 +32,33 @@ GENCODEv38_pseudogenes_to_plot <-
   geom_label_repel(aes(label = n, 
                        y = pos, size=3), 
                    show.legend = F, 
-                   nudge_x = 1)+
+                   nudge_x = 1,
+                   size = 6)+
   coord_polar("y", start = 0) +
-  scale_fill_brewer(palette = "Dark2") +
+  scale_fill_manual("Human pseudogenes", 
+                    values = c("#7570B3", "#E6AB02", "#FFFFFF"),
+                    breaks = c("Processed", "Unprocessed", "Other")) +
   theme_classic() +
   theme(legend.position="top", 
         legend.justification="top",
         legend.direction="horizontal",
-        legend.title=element_blank(),
+        legend.title=element_text(size = 16,
+                                  face = "bold"),
+        legend.text = element_text(size = 12),
         axis.text.x = element_blank(),
         axis.title = element_blank(),
         axis.line.x = element_blank(),
         axis.line.y = element_blank(), 
-        axis.ticks.y = element_blank(),
-        legend.background = element_rect(fill="gray93",
-                                         size=0.8, linetype="solid", 
-                                         colour ="black")) + 
-  guides(fill = guide_legend(nrow = 2, 
-                             byrow = T))
+        axis.ticks.y = element_blank()) + 
+  guides(fill = guide_legend(nrow = 1, 
+                             byrow = T, 
+                             title.position = "top",
+                             title.hjust = 0.5))
 
 # Save data ---------------------------------------------------------------
 
 ggsave(plot = GENCODEv38_pseudogenes_to_plot, 
-       filename = "pseudogenes_per_type.png", 
+       filename = "pseudogenes_per_type.svg", 
        path = here::here("results", "pseudogenes"), 
        width = 4, 
        height = 4, 
